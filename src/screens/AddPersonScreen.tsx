ac32fd070ebce4cd1406932fda4c367a3fa2c5ce
igorsabadini0 <igorsabadini157@gmail.com>
2026-08-05 15:12:42 -0300

    feat: add AddPersonScreen component

---
+++ src/screens/AddPersonScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { data } from "../data";
+
+export default function AddPersonScreen() {
+    return (
+        <View>
+            <Text>Igor</Text>
+        </View>
+    )
+}
